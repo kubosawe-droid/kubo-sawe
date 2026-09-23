d678985deb685d64fbbfc9c334c856c100a47959
kubosawe-droid <kubosawe@gmail.com>
2026-09-23 22:07:24 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,1 +1,8 @@
 
+export default function RootLayout({ children }: { children: React.ReactNode }){
+  return (
+    <html lang="sw">
+      <body style={{margin:0}}>{children}</body>
+    </html>
+  )
+}
